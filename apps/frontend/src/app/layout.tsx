db5e3150f520c2c173b6a851f0e85fa1f2eefbf8
prosany <mahabubsany2@gmail.com>
2026-03-23 05:21:43 +0600

    feat: update metadata and spelling for QNET in layout and settings

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -4,9 +4,9 @@ import '@/styles/globals.css';
 import { publicSans } from '@/assets/fonts';
 
 export const metadata: Metadata = {
-  title: 'BlackSignal - Intelligent Date Assignment for Indian Visa!',
+  title: 'QNET - Intelligent Date Selection for Indian Visa!',
   description:
-    'BlackSignal is an intelligent date assignment tool designed to help Indian visa applicants secure their appointments. By analyzing historical data and current trends, BlackSignal predicts the best dates for visa appointments, increasing the chances of securing a slot. With its user-friendly interface and accurate predictions, BlackSignal is the ultimate solution for anyone looking to navigate the complex process of Indian visa applications.',
+    'QNET is an intelligent date assignment tool designed to help Indian visa applicants secure their appointments. By analyzing historical data and current trends, QNET predicts the best dates for visa appointments, increasing the chances of securing a slot. With its user-friendly interface and accurate predictions, QNET is the ultimate solution for anyone looking to navigate the complex process of Indian visa applications.',
 };
 
 export default function RootLayout({
